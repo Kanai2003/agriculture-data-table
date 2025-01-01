b75abab156d412755648332c647e82b0f3e9f42f
Kanailal Manna <kanailalmanna2003@gmail.com>
2025-01-01 20:47:41 +0530

    refactor: Remove unused import and add missing import for client usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
 import { theme } from "./theme";
 import FullPageApplication from "./components/TabDataPage";
 
--- src/components/TabDataPage.tsx
+++ src/components/TabDataPage.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 import { Tabs, Card } from "@mantine/core";
 import { CropProductionExtremesTable } from "./CorpProductionExtremesTable";
